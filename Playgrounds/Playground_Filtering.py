431f9260f7667b0813971d556c93351acdb9df24
VincentDerekHeld <vincent.held@outlook.de>
2023-11-06 11:59:50 +0100

    Deleted some irrelevant information from the Project

--- Playgrounds/Playground_Filtering.py
+++
@@ -1,8 +0,0 @@
-import re
-
-
-def filter_including_sentences(text: str) -> str:
-    stop_items = [".", "!", ";"]
-    pattern = r', including[^' + ''.join(stop_items) + ']*?(?=[' + ''.join(stop_items) + '])'
-    return re.sub(pattern, '', text)
-
